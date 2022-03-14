48c5b386cea512e05c0e7caaac0ba0c6d261212a
Aidan Ryan Conner <93235716+AidanConner@users.noreply.github.com>
2022-03-13 23:22:07 -0700

    Update movies.controller.js

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -3,24 +3,24 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function list(req, res) {
     let movies;
-    if(req.query.is_showing) {
-        movies = await service.listMoviesShowing();
-    } else {
-        movies = await service.list();
-    }
-    res.json({ data: movies });
-}
+         if(req.query.is_showing) {
+              movies = await service.listMoviesShowing();
+               } else {
+                 movies = await service.list();
+            }
+           res.json({ data: movies });
+         }
 
 async function movieExists(req, res, next) {
-    const { movieId } = req.params;
-    const movie = await service.read(movieId);
-    if( movie ) {
-        res.locals.movie = movie;
-        return next();
-    } else {
-        next({ status: 404, message: "Movie cannot be found." });
-    }
-}
+            const { movieId } = req.params;
+                const movie = await service.read(movieId);
+              if( movie ) {
+                     res.locals.movie = movie;
+                return next();
+          } else {
+           next({ status: 404, message: "Movie cannot be found." });
+         }
+     }
 
 function read(req, res, next) {
     res.json({ data: res.locals.movie });
