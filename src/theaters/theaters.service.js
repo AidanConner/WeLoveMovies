4bac085709ba3aaaafeddc3079d8193a2cc3183d
Aidan Ryan Conner <93235716+AidanConner@users.noreply.github.com>
2022-03-13 23:25:48 -0700

    Update theaters.service.js

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,7 +1,7 @@
 const knex = require("../db/connection");
 
-async function attachMovies(theater) {
-    theater.movies = await knex("movies")
+   async function attachMovies(theater) {
+          theater.movies = await knex("movies")
         .join("movies_theaters", "movies.movie_id", "movies_theaters.movie_id")
         .where({ "movies_theaters.theater_id": theater.theater_id });
     return theater;
@@ -12,13 +12,13 @@ function list() {
         return Promise.all(
             theaters.map(theater => {
                 return attachMovies(theater);
-            })
-        );
-    });
-}
+              })
+          );
+      });
+          }
 
-function listByMovie(movieId) {
-    return knex("theaters")
+        function listByMovie(movieId) {
+        return knex("theaters")
         .join("movies_theaters", "theaters.theater_id", "movies_theaters.theater_id")
         .select("theaters.*", "movies_theaters.is_showing", "movies_theaters.movie_id")
         .where({ movie_id: movieId });
