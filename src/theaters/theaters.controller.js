89a689153317ae0b316db9c4938666b0b22c5fbb
Aidan Ryan Conner <93235716+AidanConner@users.noreply.github.com>
2022-03-13 23:24:10 -0700

    Update theaters.controller.js

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -2,15 +2,15 @@ const service = require("./theaters.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function list(req, res) {
-    let theaters;
-    if(res.locals.movie) {
-        const movieId = res.locals.movie.movie_id;
-        theaters = await service.listByMovie(movieId);
-    } else {
-        theaters = await service.list();
-    }
-    res.json({ data: theaters });
-}
+     let theaters;
+         if(res.locals.movie) {
+            const movieId = res.locals.movie.movie_id;
+            theaters = await service.listByMovie(movieId);
+           } else {
+           theaters = await service.list();
+       }
+     res.json({ data: theaters });
+ }
 
 module.exports = {
     list: asyncErrorBoundary(list),
